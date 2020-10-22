9921bebf918659065f721e3b6a3b3559318c9fab
Fagner Cunha <fagner.cunha99@gmail.com>
2020-10-22 00:32:38 -0400

    Added random crop to the preprocessig pipeline

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,39 @@ import tensorflow as tf
 
 import randaugment
 
+def random_crop(image,
+                aspect_ratio_range=[0.75, 1.33],
+                area_range=[0.65, 1],
+                min_object_covered=0.5,
+                max_attempts=100):
+
+  bbox = tf.constant([0.0, 0.0, 1.0, 1.0], dtype=tf.float32, shape=[1, 1, 4])
+  bbox_begin, bbox_size, _ = tf.image.sample_distorted_bounding_box(
+      tf.shape(image),
+      bounding_boxes=bbox,
+      min_object_covered=min_object_covered,
+      area_range=area_range,
+      aspect_ratio_range=aspect_ratio_range,
+      use_image_if_no_bounding_boxes=True,
+      max_attempts=max_attempts
+  )
+
+  offset_height, offset_width, _ = tf.unstack(bbox_begin)
+  target_height, target_width, _ = tf.unstack(bbox_size)
+
+  image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+  image = tf.image.crop_to_bounding_box(
+    image,
+    offset_height,
+    offset_width,
+    target_height,
+    target_width
+  )
+
+  return image
+
 def flip(image):
-  return tf.image.flip_left_right(image)
+  return tf.image.random_flip_left_right(image)
 
 def normalize_image(image):
   image = tf.image.convert_image_dtype(image, dtype=tf.float32)
@@ -24,10 +55,10 @@ def preprocess_for_train(image,
                         output_size,
                         randaug_num_layers=None,
                         randaug_magnitude=None):
+
+  image = random_crop(image)
+  image = tf.image.convert_image_dtype(image, dtype=tf.float32)
   image = tf.image.resize(image, size=(output_size, output_size))
-
-  #random crop
-
   image = flip(image)
 
   if randaug_num_layers is not None and randaug_magnitude is not None:
@@ -42,11 +73,7 @@ def preprocess_for_train(image,
 
 def preporocess_for_eval(image, output_size):
 
-  # (center) crop
-  #resize
-
   image = tf.image.resize(image, size=(output_size, output_size))
-
   image = normalize_image(image)
 
   return image
