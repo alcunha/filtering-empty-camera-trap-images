25381d25479b036f7913a63504e1086c04039b2a
Fagner Cunha <fagner.cunha99@gmail.com>
2020-10-24 15:56:46 -0400

    Added ModelSpecs class to the model builder

--- model_builder.py
+++ model_builder.py
@@ -1,12 +1,30 @@
+import collections
+
 import tensorflow as tf
 import tensorflow_hub as hub
 
-efficientnet_b0_spec = lambda: dict(
+class ModelSpecs(
+  collections.namedtuple("ModelSpecs", [
+    'uri', 'input_size', 'classes', 'activation'
+  ])):
+  """ The specifications for a image model
+  """
+
+
+def get_default_specs():
+  return ModelSpecs(
+    uri='https://tfhub.dev/tensorflow/efficientnet/b0/feature-vector/1',
+    input_size=224,
+    classes=2,
+    activation='softmax'
+  )
+
+efficientnet_b0_spec = get_default_specs()._replace(
   uri='https://tfhub.dev/tensorflow/efficientnet/b0/feature-vector/1',
   input_size=224
 )
 
-efficientnet_lite0_spec = lambda: dict(
+efficientnet_lite0_spec = get_default_specs()._replace(
   uri='https://tfhub.dev/tensorflow/efficientnet/lite0/feature-vector/2',
   input_size=224
 )
@@ -18,12 +36,11 @@ MODELS_SPECS = {
 
 def _create_model_from_specs(specs):
   model = tf.keras.Sequential([
-    hub.KerasLayer(specs['uri'], trainable=True),
-    tf.keras.layers.Dense(units=specs['classes'],
-                          activation=specs['activation'])
+    hub.KerasLayer(specs.uri, trainable=True),
+    tf.keras.layers.Dense(units=specs.classes, activation=specs.activation)
   ])
 
-  model.build([None, specs['input_size'], specs['input_size'], 3])
+  model.build([None, specs.input_size, specs.input_size, 3])
 
   return model
 
@@ -36,11 +53,12 @@ def create(model_name,
   if model_name not in MODELS_SPECS.keys():
     raise RuntimeError('Model %s not implemented' % model_name)
 
-  specs = MODELS_SPECS[model_name]()
-  custom_specs = dict(classes=num_classes,
-                      activation=classifier_activation)
-  specs.update(custom_specs)
+  specs = MODELS_SPECS[model_name]
+  specs = specs._replace(
+    classes=num_classes,
+    activation=classifier_activation,
+  )
   if input_size is not None:
-    specs.update({'input_size': input_size})
-  
+    specs = specs._replace(input_size=input_size)
+
   return _create_model_from_specs(specs)
