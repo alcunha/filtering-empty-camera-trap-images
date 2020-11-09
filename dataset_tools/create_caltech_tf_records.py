8c867b1b9abeed112ebdad7e677bb83272477073
Fagner Cunha <fagner.cunha99@gmail.com>
2020-11-09 16:23:58 -0400

    Added random seed and validation to bbox going out of images

--- dataset_tools/create_caltech_tf_records.py
+++ dataset_tools/create_caltech_tf_records.py
@@ -1,4 +1,5 @@
 import os
+import random
 import hashlib
 import json
 import contextlib2
@@ -58,9 +59,20 @@ flags.DEFINE_string(
 )
 
 flags.DEFINE_integer(
-    'images_per_shard', default=800,
+    'images_per_shard', default=600,
     help=('Number of images per shard')
 )
+
+flags.DEFINE_bool(
+    'shufle_images', default=True,
+    help=('Shufle images before to write to tfrecords')
+)
+
+if 'random_seed' not in list(FLAGS):
+  flags.DEFINE_integer(
+      'random_seed', default=42,
+      help=('Random seed for reproductible experiments')
+  )
 
 flags.mark_flag_as_required('images_file')
 flags.mark_flag_as_required('annotations_file')
@@ -89,6 +101,9 @@ def create_tf_example(image,
   image_id = image['id']
 
   image_path = os.path.join(dataset_base_dir, filename)
+  if not tf.io.gfile.exists(image_path):
+    return None, num_annotations_skipped, num_empty_annotations_skipped
+
   with tf.io.gfile.GFile(image_path, 'rb') as image_file:
     encoded_image_data = image_file.read()
   key = hashlib.sha256(encoded_image_data).hexdigest()
@@ -111,12 +126,20 @@ def create_tf_example(image,
     (x, y, bbox_width, bbox_height) = tuple(annotation['bbox'])
     category_id = annotation['category_id']
 
-    if bbox_width <=0 or bbox_height <= 0 or x < 0 or y < 0:
+    if bbox_width <= 0 or bbox_height <= 0:
       num_annotations_skipped += 1
       continue
-    if x + bbox_width > orig_width or y + bbox_height > orig_height:
-      num_annotations_skipped += 1
-      continue
+
+    if x < 0:
+      bbox_width = bbox_width + x
+      x = 0
+    if y < 0:
+      bbox_height = bbox_height + y
+      y = 0
+    if x + bbox_width > orig_width:
+      bbox_width = orig_width - x
+    if y + bbox_height > orig_height:
+      bbox_height = orig_height - y
 
     if FLAGS.label_type=='empty':
       origin_category_text = original_category_index[category_id]['name']
@@ -162,6 +185,7 @@ def create_tf_record_from_images_list(images,
   num_shards = 1 + (len(images) // FLAGS.images_per_shard)
   total_annot_skipped = 0
   total_empty_annot_skipped = 0
+  total_image_skipped = 0
 
   with contextlib2.ExitStack() as tf_record_close_stack:
     output_tfrecords = tf_record_creation_util.open_sharded_output_tfrecords(
@@ -180,13 +204,18 @@ def create_tf_record_from_images_list(images,
       total_annot_skipped += annot_skipped
       total_empty_annot_skipped += empty_annot_skipped
 
-      output_shard_index = index % num_shards
-      output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
+      if tf_example is not None:
+        output_shard_index = index % num_shards
+        output_tfrecords[output_shard_index].write(
+            tf_example.SerializeToString())
+      else:
+        total_image_skipped += 1
 
     tf.compat.v1.logging.info('Finished writing, skipped %d bboxes.',
                                total_annot_skipped)
     tf.compat.v1.logging.info('Skipped %d bboxes on empty images.', 
                               total_empty_annot_skipped)
+    tf.compat.v1.logging.info('%d images not found.', total_image_skipped)
 
 def _get_caltech_splits_by_location(caltech_splits_file):
   with tf.io.gfile.GFile(caltech_splits_file, 'r') as json_file:
@@ -246,6 +275,10 @@ def _create_caltech_tf_record_splits(caltech_images_file,
   for split, images in images_per_split.items():
     tf.compat.v1.logging.info('Started writing %s split.', split)
     tfrecord_path = os.path.join(output_path, 'caltech_%s.record' % split)
+
+    if FLAGS.shufle_images:
+      random.shuffle(images)
+
     create_tf_record_from_images_list(images,
                                       annot_index,
                                       dataset_base_dir,
@@ -253,9 +286,15 @@ def _create_caltech_tf_record_splits(caltech_images_file,
                                       origin_cat_index,
                                       tfrecord_path)
 
+def set_random_seeds():
+  random.seed(FLAGS.random_seed)
+  tf.random.set_seed(FLAGS.random_seed)
+
 def main(_):
   if FLAGS.label_type=='species':
     raise RuntimeError('species option for flag --label_type not implemented')
+
+  set_random_seeds()
 
   _create_caltech_tf_record_splits(FLAGS.images_file,
                                    FLAGS.annotations_file,
