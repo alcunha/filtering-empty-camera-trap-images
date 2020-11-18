31e9cee6ba18ade62f0bf31633053fe0880142fe
Fagner Cunha <fagner.cunha99@gmail.com>
2020-11-17 22:42:34 -0400

    Updated training flags to suport both csv and tfrecords files

--- main.py
+++ main.py
@@ -13,17 +13,30 @@ import utils
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string(
-    'training_csv_file', default=None,
-    help=('CSV file containing the list of images for training'
-          ' (CSV file must have two columns: filename and category id)'))
+    'training_files', default=None,
+    help=('A file pattern for TFRecord files OR a CSV file containing the list'
+          ' of images for training (CSV file must have two columns: filename'
+          ' and category id)'))
+
+flags.DEFINE_integer(
+    'num_training_instances', default=None,
+    help=('Number of training instances')
+)
+
 flags.DEFINE_string(
-    'validation_csv_file', default=None,
-    help=('CSV file containing the list of images for evaluation'
-          ' (CSV file must have two columns: filename and category id)'))
+    'validation_files', default=None,
+    help=('A file pattern for TFRecord files OR a CSV file containing the list'
+          ' of images for evaluation (CSV file must have two columns: filename'
+          ' and category id)'))
+
+flags.DEFINE_integer(
+    'num_validation_instances', default=None,
+    help=('Number of validation instances')
+)
 
 flags.DEFINE_string(
     'dataset_base_dir', default=None,
-    help=('Path to images dataset base directory'))
+    help=('Path to images dataset base directory when using a CSV file'))
 
 flags.DEFINE_integer(
     'input_size', default=224,
@@ -51,7 +64,7 @@ flags.DEFINE_string(
     help=('Model name of the archtecture'))
 
 flags.DEFINE_string(
-    'model_dir', default='/tmp/models/',
+    'model_dir', default=None,
     help=('Location of the model checkpoint files'))
 
 flags.DEFINE_integer(
@@ -96,12 +109,37 @@ if 'random_seed' not in list(FLAGS):
       help=('Random seed for reproductible experiments')
   )
 
+flags.mark_flag_as_required('training_files')
+flags.mark_flag_as_required('model_dir')
 
-def build_input_data(csv_file, is_training=False):
+def build_csv_input_data(csv_file, is_training=False):
+  if FLAGS.dataset_base_dir is None:
+    raise RuntimeError('To use CSV files as input, you must specify'
+                       ' --dataset_base_dir')
+
   input_data = dataloader.CSVInputProcessor(
     csv_file=csv_file,
     data_dir=FLAGS.dataset_base_dir,
     batch_size=FLAGS.batch_size,
+    is_training=is_training,
+    output_size=FLAGS.input_size,
+    randaug_num_layers=FLAGS.randaug_num_layers,
+    randaug_magnitude=FLAGS.randaug_magnitude,
+    seed=FLAGS.random_seed,
+  )
+
+  return input_data.make_source_dataset()
+
+def build_tfrecord_input_data(file_pattern, num_instances, is_training=False):
+  if FLAGS.num_classes is None:
+    raise RuntimeError('To use TFRecords as input, you must specify'
+                       ' --num_classes')
+
+  input_data = dataloader.TFRecordWBBoxInputProcessor(
+    file_pattern=file_pattern,
+    batch_size=FLAGS.batch_size,
+    num_classes=FLAGS.num_classes,
+    num_instances=num_instances,
     is_training=is_training,
     output_size=FLAGS.input_size,
     randaug_num_layers=FLAGS.randaug_num_layers,
@@ -156,15 +194,6 @@ def set_random_seeds():
   tf.random.set_seed(FLAGS.random_seed)
 
 def main(_):
-  if FLAGS.training_csv_file is None:
-    raise RuntimeError('Must specify --training_csv_file for train.')
-
-  if FLAGS.dataset_base_dir is None:
-    raise RuntimeError('Must specify --dataset_base_dir for train.')
-
-  if FLAGS.model_dir is None:
-    raise RuntimeError('Must specify --model_dir for train.')
-
   if utils.xor(FLAGS.randaug_num_layers is None,
                FLAGS.randaug_magnitude is None):
     raise RuntimeError('To apply Randaugment during training you must specify'
@@ -172,16 +201,38 @@ def main(_):
 
   set_random_seeds()
 
-  dataset, num_instances, num_classes = build_input_data(
-    FLAGS.training_csv_file,
-    is_training=True
-  )
-
-  if FLAGS.validation_csv_file is not None:
-    val_dataset, val_num_instances, _ = build_input_data(
-      FLAGS.validation_csv_file,
-      is_training=False
+  if FLAGS.training_files.endswith('.csv'):
+    dataset, num_instances, num_classes = build_csv_input_data(
+      FLAGS.training_files,
+      is_training=True
     )
+  else:
+    if FLAGS.num_training_instances is None:
+      raise RuntimeError('Must specify --num_training_instances when using'
+                         'TFREcords for training')
+
+    dataset, num_instances, num_classes = build_tfrecord_input_data(
+      FLAGS.training_files,
+      FLAGS.num_training_instances,
+      is_training=True
+    )
+
+  if FLAGS.validation_files is not None:
+    if FLAGS.validation_files.endswith('.csv'):
+      val_dataset, val_num_instances, _ = build_csv_input_data(
+        FLAGS.validation_files,
+        is_training=False
+      )
+    else:
+      if FLAGS.num_validation_instances is None:
+        raise RuntimeError('Must specify --num_validation_instances when using'
+                          'TFREcords for validation')
+
+      val_dataset, val_num_instances, _ = build_tfrecord_input_data(
+        FLAGS.validation_files,
+        FLAGS.num_validation_instances,
+        is_training=False
+      )
   else:
     val_dataset = None
     val_num_instances = 0
